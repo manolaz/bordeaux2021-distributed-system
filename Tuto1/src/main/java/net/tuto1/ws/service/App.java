c4ec4e99d591cea6dc1dab487e8caeee189ec884
Tristan Nguyen <trungupdate@gmail.com>
2021-03-16 22:07:21 +0700

    day 1

---
+++ Tuto1/src/main/java/net/tuto1/ws/service/App.java
@@ -0,0 +1,13 @@
+package net.tuto1.ws.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
